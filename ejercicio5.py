ea337a182c57efe5854b658d8045d248317b3d01
THEGABOALE <garciaga@uamv.edu.ni>
2025-03-19 12:27:13 -0600

    nuevo

---
+++ ejercicio5.py
@@ -0,0 +1,14 @@
+print("Ingrese el número de trabajadores a recibir su salario: ", end="\n")
+numtrabajadores = int(input())
+print("Ingrese el pago a dar por cada hora laboral realizada: ", end="\n")
+salarios = float(input())
+pago = list()
+for i in range(numtrabajadores):
+    horas = int(input(f"Ingrese las horas realizadas del trabajador {i+1}:"))
+    if int(horas) <= 48:
+    horas*salarios
+    else int(horas) > 48:
+    48 * salarios + (horas - 48) * salarios * 2
+    
+
+
