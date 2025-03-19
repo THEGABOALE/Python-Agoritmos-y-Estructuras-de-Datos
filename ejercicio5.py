7658ab7bd77d94a0df957a123552a15c3a041c86
THEGABOALE <garciaga@uamv.edu.ni>
2025-03-19 12:30:30 -0600

    vamoooss

--- ejercicio5.py
+++ ejercicio5.py
@@ -6,9 +6,9 @@ pago = list()
 for i in range(numtrabajadores):
     horas = int(input(f"Ingrese las horas realizadas del trabajador {i+1}:"))
     if int(horas) <= 48:
-    horas*salarios
+    pago=horas*salarios
     else int(horas) > 48:
-    48 * salarios + (horas - 48) * salarios * 2
+    pago=48 * salarios + (horas - 48) * salarios * 2
     
 
 
