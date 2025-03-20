9f66bf734076e0968871329243b9efb68d142580
THEGABOALE <garciaga@uamv.edu.ni>
2025-03-20 00:04:55 -0600

    cambiazo

--- ejercicio5.py
+++ ejercicio5.py
@@ -4,11 +4,20 @@ print("Ingrese el pago a dar por cada hora laboral realizada: ", end="\n")
 salarios = float(input())
 pago = list()
 for i in range(numtrabajadores):
-    horas = int(input(f"Ingrese las horas realizadas del trabajador {i+1}:"))
-    if int(horas) <= 48:
-    pago=horas*salarios
-    else int(horas) > 48:
-    pago=48 * salarios + (horas - 48) * salarios * 2
-    
-
+    while True:
+        try:
+            horas = int(input(f"Ingrese las horas realizadas del trabajador {i+1}:"))
+            if horas < 0:
+                print("Las horas no pueden ser negativas. Intentelo de nuevo.",endl="\n")
+                continue
+            break
+        except ValueError:
+            print("Error. Ingrese un número válido.")
+    if horas <= 48:
+        pago.append(horas*salarios)
+    else:
+        pago.append(48 * salarios + (horas - 48) * salarios * 2)
+print("\nSalarios de los trabajadores:")
+for i in range(numtrabajadores):
+    print(f"Trabajador {i+1}: ${pago[i]}")
 
