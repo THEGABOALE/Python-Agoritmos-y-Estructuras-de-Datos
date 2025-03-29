ea38844d47b94a94be647f8f10c0474b3e4044d5
THEGABOALE <garciaga@uamv.edu.ni>
2025-03-28 19:15:21 -0600

    terminado

--- semana 2/operadoresmatematicos1.py
+++ semana 2/operadoresmatematicos1.py
@@ -1,3 +1,8 @@
+'''
+Programa que solicita dos números enteros y da como resultado 
+cada uno de los operadores matemáticos básicos.
+'''
+
 print("Escribe un número: ",end="\n")
 num1=int(input())
 print("Escribe otro número ",end="\n")
@@ -12,4 +17,4 @@ print("\nRESULTADO DE TODOS LOS OPERADORES MATEMÁTICOS: ",end="\n")
 print("\nSUMA DE LOS DOS NÚMEROS: ",suma)
 print("\nRESTA DE LOS DOS NÚMEROS: ",resta)
 print("\nMULTIPLICACIÓN DE LOS DOS NÚMEROS: ",multiplicacion)
-print("\nDIVISIÓN DE LOS DOS NÚMEROS: ",division)
+print("\nDIVISIÓN DE LOS DOS NÚMEROS: ",division,end="\n")
--- semana 2/operadoresmatematicos2.py
+++ semana 2/operadoresmatematicos2.py
@@ -1,3 +1,13 @@
-print("Ingrese un número: ",end="\n")
-num=int(input())
+'''
+Programa para calcular la quinta potencia, la raíz cuadrada, el exponencial, 
+el logaritmo natural y el valor absoluto de un número introducido por el teclado.
+'''
+import math
 
+print("Ingrese un número: ",end="\n")
+num=float(input())
+print("El resultado de la quinta potencia es: ",num**5)
+print("El resultado de la raíz cuadrada es: ",math.sqrt(num))
+print("EL resultado de la exponencial es: ",math.exp(num))
+print ("El resultado del logaritmo natural es: ",math.log(num))
+print("El resultado del valor absoluto es: ",abs(num))
