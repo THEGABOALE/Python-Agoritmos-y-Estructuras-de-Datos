9e3bc3d830183e5c1aeb0581c62358f808170827
THEGABOALE <garciaga@uamv.edu.ni>
2025-03-31 08:49:04 -0600

    codificando

---
+++ semana 3/triangulo.py
@@ -0,0 +1,21 @@
+'''
+Desarrollar un programa que cargue los datos de un triángulo.
+Implementar un método/función para determinar el tipo de triángulo
+(equilátero, isósceles o escaleno).
+'''
+
+def main():
+    print("==MEDIDA DE CADA LADO DEL TRIÁNGULO==")
+    lado1 = int(input("\nIngrese la medida del lado 1: "))
+    lado2 = int(input("\nIngrese la medida del lado 2: "))
+    lado3 = int(input("\nIngrese la medida del lado 3: "))
+    
+    if lado1 == lado2 == lado3:
+        print("\nEl triángulo es equilatero")
+    elif lado1 == lado2 or lado2 == lado3 or lado1 == lado3:
+        print("\nEl triángulo es isósceles.")
+    else:
+        print("\nEl triángulo es escaleno.")
+
+if __name__ == "__main__":
+    main()
