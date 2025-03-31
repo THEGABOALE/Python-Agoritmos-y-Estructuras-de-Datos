0571e97b395f506976c2f91ec9b99842cf761bd0
THEGABOALE <garciaga@uamv.edu.ni>
2025-03-31 17:05:10 -0600

    terminado

--- semana 2/problema2.py
+++ semana 2/problema2.py
@@ -9,38 +9,36 @@ while True:
     print("3. Retirar dinero")
     print("4. Salir")
     
-    opcion = input("Seleccione una opción: ")
+    opcion = input("\nSeleccione una opción: ")
     
     if opcion == '1':
-        print(f"Su saldo actual es: {saldo:.2f}")
+        print(f"\nSu saldo actual es: ${saldo:.2f}")
     elif opcion == '2':
         try:
-            deposito = float(input("Ingrese la cantidad a depositar: "))
+            deposito = float(input("\nIngrese la cantidad a depositar ($): "))
             if deposito > 0:
                 saldo += deposito
-                print(f"Depósito exitoso. Su nuevo saldo ahora es: {saldo:.2f}")
+                print(f"\nDepósito exitoso. Su nuevo saldo ahora es de: ${saldo:.2f}")
             else:
-                print("La cantidad a depositar debe ser mayor a 0.")
+                print("\nLa cantidad a depositar debe ser mayor a 0.")
         except:
-            print("La entrada no es válida.")
+            print("\nLa entrada no es válida.")
     elif opcion == '3':
         try:
-            retiro = float(input("Ingrese la cantidad retirar: "))
+            retiro = float(input("\nIngrese la cantidad retirar ($): "))
             if retiro > 0:
                 if retiro <= saldo:
                     saldo -= retiro
-                    print(f"Retiro exitoso. Su nuevo saldo ahora es: {saldo:.2f}")
+                    print(f"\nRetiro exitoso. Su nuevo saldo ahora es de: ${saldo:.2f}")
                 else:
-                    print("Saldo insuficiente.")
+                    print("\nSaldo insuficiente.")
             else:
-                print("La cantidad debe ser mayor a cero.")
+                print("\nLa cantidad debe ser mayor a cero.")
         except ValueError:
-            print("La entrada no es válida.")
+            print("\nLa entrada no es válida.")
     elif opcion == '4':
-        print("Saliendo de cuenta bancaria...")
-        print("¡Que tenga un buen día usuario!")
+        print("\nSaliendo de la cuenta bancaria...")
+        print("\n¡Que tenga un buen día usuario!")
         break
     else:
-        print("Opción no válida. Inténtelo de nuevo.")
-        
-        
+        print("\nOpción no válida. Inténtelo de nuevo.")
