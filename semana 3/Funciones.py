c3e30adafe86be8089e99f94ea591cbdc9fd23d7
THEGABOALE <garciaga@uamv.edu.ni>
2025-03-31 07:43:45 -0600

    codificando

---
+++ semana 3/Funciones.py
@@ -0,0 +1,12 @@
+'''Ejercicios de funciones en python'''
+#Definición de Funciones
+
+#Programa principal
+def main():
+    lista = []
+    print = ("Ingrese la cantidad de elementos a procesar: ", end='')
+    n = int(input())
+    
+    for i in range(n):
+        print(f"Ingrese el elemento {i+1}: ", end='')
+    
