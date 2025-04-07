3db631640ceef1d23643adb3ec07db37b74e3284
THEGABOALE <garciaga@uamv.edu.ni>
2025-04-07 09:15:22 -0600

    cambio

--- semana 4/clase_gastos.py
+++
@@ -1,9 +0,0 @@
-class Gastos:
-    def __init__(self, fecha, categoria, monto, descripcion):
-        self.__fecha = fecha
-        self.__categoria = categoria
-        self.__monto = monto
-        self.__descripcion = descripcion
-    
-def __str__(self):
-    return f"Fecha: {self.__fecha} | Categoría: {self.__categoria} | Monto: ${self.__monto} | Descripción: {self.__descripcion}"
