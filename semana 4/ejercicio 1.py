988b01582f8b3fa6560eb7ce29328fbdd0c4383f
THEGABOALE <garciaga@uamv.edu.ni>
2025-04-16 23:44:21 -0600

    ejercicios prácticos

---
+++ semana 4/ejercicio 1.py
@@ -0,0 +1,3 @@
+'''Implementar un método que recibe una lista de enteros L y un número entero n de 
+forma que modifique la lista mediante el borrado de todos los elementos de la lista 
+que tengan este valor.'''
---
+++ semana 4/ejercicio 2.py
@@ -0,0 +1,2 @@
+'''Construir un método cantVocales que determine la cantidad de vocales 
+almacenadas en una lista de caracteres.'''
