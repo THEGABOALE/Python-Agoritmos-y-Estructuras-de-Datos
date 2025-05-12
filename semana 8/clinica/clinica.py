2a4e4a26070fbbd586f8d8de56759571fc2a5bbb
THEGABOALE <garciaga@uamv.edu.ni>
2025-05-12 07:58:08 -0600

    Simulación de clínica

--- semana 8/clinica/clinica.py
+++ semana 8/clinica/clinica.py
@@ -5,14 +5,13 @@ class Clinica:
         self.pacientes = []
     
     def registrar(self, nombre):
-        nombre = input("Ingrese su el nombre del paciente: ")
         self.pacientes.append(nombre)
         
     def atender(self):
         if self.pacientes:
             atendido = self.pacientes.pop(0)
             print(f'''El paciente {atendido} ha sido atentido. Vuelva pronto y más herido que gano más plata para apostarlo 
-                  todo en el negro en el casino más cercano en las Vegas.''')
+                todo en el negro en el casino más cercano en las Vegas.''')
         else:
             print("No hay pacientes en la lista de espera.")
     
@@ -23,3 +22,30 @@ class Clinica:
                 print(f"{i}. {paciente}")
         else:
             print("No hay pacientes en la lista de espera.")
+
+def menu():
+    clinica = Clinica()
+    while True:
+        print("\n--Menú principal--\n")
+        print("1. Registrar")
+        print("2. Atender")
+        print("3. Imprimir")
+        print("4. Salir")
+        
+        opcion = int(input("\nSeleccione una de las siguientes opciones: "))
+        
+        if opcion == 1:
+            nombre = input("\nIngrese el nombre del paciente: ")
+            clinica.registrar(nombre)
+        elif opcion == 2:
+            clinica.atender()
+        elif opcion == 3:
+            clinica.imprimir()
+        elif opcion == 4:
+            print("\nSaliendo de la clínica... ¡Vuelva pronto!")
+            break
+        else:
+            print("\nVuelva a intentarlo nuevamente, favor seleccionar una opción válida.")
+
+if __name__ == '__main__':
+    menu()
