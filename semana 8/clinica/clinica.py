c4e9461cf512c0931fc502fc429f214078777361
THEGABOALE <garciaga@uamv.edu.ni>
2025-05-12 08:05:27 -0600

    Simulación de clínica

--- semana 8/clinica/clinica.py
+++ semana 8/clinica/clinica.py
@@ -10,18 +10,17 @@ class Clinica:
     def atender(self):
         if self.pacientes:
             atendido = self.pacientes.pop(0)
-            print(f'''El paciente {atendido} ha sido atentido. Vuelva pronto y más herido que gano más plata para apostarlo 
-                todo en el negro en el casino más cercano en las Vegas.''')
+            print(f'\nEl paciente {atendido} ha sido atentido.')
         else:
-            print("No hay pacientes en la lista de espera.")
+            print("\nNo hay pacientes en la lista de espera.")
     
     def imprimir(self):
         if self.pacientes:
-            print("Lista de pacientes en espera de atención")
+            print("\nLista de pacientes en espera de atención")
             for i, paciente in enumerate(self.pacientes, start = 1):   
                 print(f"{i}. {paciente}")
         else:
-            print("No hay pacientes en la lista de espera.")
+            print("\nNo hay pacientes en la lista de espera.")
 
 def menu():
     clinica = Clinica()
